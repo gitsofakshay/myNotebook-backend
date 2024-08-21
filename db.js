59e1c68d1b2035d81e8c6f08cbf547df1437ec7a
AKSHAY RAJ <146107334+gitsofakshay@users.noreply.github.com>
2024-08-21 19:26:47 +0530

    connected to mongodb atlas

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-// const mongoURI = ' mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.10';
-const mongoURI = 'mongodb://localhost:27017/mynotebook';
+// const mongoURI = 'mongodb://localhost:27017/mynotebook';
+require('dotenv').config();
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI,)
+    mongoose.connect(process.env.ATLAS_URI)
     .then(()=> console.log('Connected to mongodb successfully'))
     .catch(err => console.error('Could not connecct to mongodb:',err));
 }

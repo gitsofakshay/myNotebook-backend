1001004250b0eecf89228df4578284708cfb92a1
AKSHAY RAJ <146107334+gitsofakshay@users.noreply.github.com>
2024-08-22 10:28:52 +0530

    db.js file is updated

--- db.js
+++ db.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();  // Ensure this is at the top
-
+const mongoURI = process.env.ATLAS_URI;
 const connectToMongo = () => {
-    mongoose.connect(process.env.ATLAS_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('Connected to MongoDB successfully'))
+    mongoose.connect(mongoURI)
+    .then(() => console.log('Connected to MongoDB Atlas successfully'))
     .catch(err => console.error('Could not connect to MongoDB:', err));
 };
-
 module.exports = connectToMongo;

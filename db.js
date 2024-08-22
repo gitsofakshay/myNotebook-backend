d92ed547ee3e33809f0c29feaa5dc45efd8035b1
AKSHAY RAJ <146107334+gitsofakshay@users.noreply.github.com>
2024-08-22 10:00:58 +0530

    port is defined

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
-// const mongoURI = 'mongodb://localhost:27017/mynotebook';
-require('dotenv').config();
-const connectToMongo = ()=>{
-    mongoose.connect(process.env.ATLAS_URI)
-    .then(()=> console.log('Connected to mongodb successfully'))
-    .catch(err => console.error('Could not connecct to mongodb:',err));
-}
+require('dotenv').config();  // Ensure this is at the top
+
+const connectToMongo = () => {
+    mongoose.connect(process.env.ATLAS_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('Connected to MongoDB successfully'))
+    .catch(err => console.error('Could not connect to MongoDB:', err));
+};
 
 module.exports = connectToMongo;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const connectToMongo = require('./db');
 const cors = require('cors');
 connectToMongo();
 const app = express();
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3000;
  
 app.use(cors())
 

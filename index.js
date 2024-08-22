6641ea9276ed6205146abfc31bcbc83ec9a7d10b
AKSHAY RAJ <146107334+gitsofakshay@users.noreply.github.com>
2024-08-22 15:10:27 +0530

    code is updated for keep server alive

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authentication.js'));
 app.use('/api/notes', require('./routes/notes.js'));
 app.use('/api/forgetpwd', require('./routes/forgetpassword.js'));
+// Keep-alive endpoint
+app.get('/api/keep-alive', (req, res) => {
+  res.status(200).json({ message: 'Server is alive' });
+});
 
 app.listen(port, () => {
   console.log(`myNotebook Server is running on http://localhost:${port}`);
